0292c9723878e94f91a7c4e8968769ecbfd7c4e6
Sohail Raoufi <sohailraufi19@gmail.com>
2025-02-08 21:53:17 +0430

    lcm

--- codeChef/number-theory/gcd.py
+++ codeChef/number-theory/gcd.py
@@ -4,8 +4,18 @@ def gcd(a, b):
     return gcd(b % a, a)
 
 
-a = 35
-b = 10
+def lcm(a, b):
+    gcd_a_b = gcd(a, b)
+    res = (a * b) // gcd_a_b
+
+    return res
+
+
+
+
+a = 12
+b = 15
 
 
 print(gcd(a, b))
+print(lcm(a, b))
